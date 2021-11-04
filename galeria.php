f5e9a5077235e034c4960cd28e95a0e48f5b74df
Fromthevoid17 <93692892+Fromthevoid17@users.noreply.github.com>
2021-11-03 23:05:58 -0500

    Update galeria.php

--- galeria.php
+++ galeria.php
@@ -1,9 +1,5 @@
 <!DOCTYPE HTML>
-<!--
-	Binary by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
--->
+
 <html>
 	<head>
 		<title>Galeríá</title>
@@ -66,14 +62,6 @@
 					
 		<!-- Footer -->
 			<footer id="footer">
-				<ul class="icons">
-					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-					<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-					<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-				</ul>
-				<div class="copyright">
-					&copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.
-				</div>
 			</footer>
 
 		<!-- Scripts -->
